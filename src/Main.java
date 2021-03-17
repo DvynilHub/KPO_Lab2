6f3e6980695c8f93d20971c3063ebe0faa45f34d
DvynilHub <belonogov220@gmail.com>
2021-03-17 22:20:22 +0800

    Lab2_3

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,78 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        String fileName = "123.txt";
-        String search = "public";
-        String replace = "private";
-        Charset charset = StandardCharsets.UTF_8;
-        Path path = Paths.get(fileName);
-        try {
-            Files.write(path, new String(Files.readAllBytes(path), charset).replace(search, replace).getBytes(charset));
-            System.out.println("Замена произведена");
-        } catch (NoSuchFileException e){
-            System.out.println("Файл не найден");
+    public static void main(String[] args) throws IOException {
+        double[][] matrix = matr();
+        trans(matrix);
 
+    }
+    private static void trans(double[][] a){  //метод транспонирования и вывода матрицы
+        try {
+            for (int i = 0; i < a.length; i++) {
+                for (int j = i + 1; j < a.length; j++) {
+                    double temp = a[i][j];
+                    a[i][j] = a[j][i];
+                    a[j][i] = temp;
+                }
+            }
+            System.out.println();
+            System.out.println("Транспонированная матрица"+"\n-------------------------");
+            for (double[] doubles : a) {
+                for (int j = 0; j < a.length; j++) {
+                    System.out.printf("%8.2f",doubles[j]);
+                }
+                System.out.println();
+            }
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Матрицу невозможно транспонировать");
+        } catch (NullPointerException e){
+            System.out.println("Неверный формат матрицы");
         }
 
     }
+    private static double[][] matr() throws IOException { //функция считывания матрицы из файла
+        BufferedReader br = new BufferedReader(new FileReader("123.txt"));
+        try {
+            List<String> lines = new ArrayList<>();
+            while (br.ready()) {
+                lines.add(br.readLine());
+            }
+            int matrixWidth = lines.get(0).split(" ").length;
+            int matrixHeight = lines.size();
+            if (matrixHeight != matrixWidth){
+                return null;
+            }
+
+            double[][] matrix = new double[matrixHeight][matrixWidth];
+
+            for (int i = 0; i < matrixHeight; i++) {
+                for (int j = 0; j < matrixWidth; j++) {
+                    String[] line = lines.get(i).split(" ");
+                    matrix[i][j] = Double.parseDouble(line[j]);
+                }
+            }
+            System.out.println("Исходная матрица"+"\n-------------------------");
+            for (int i = 0; i < matrixHeight; i++) {
+                for (int j = 0;j<matrixWidth;j++){
+                    System.out.printf("%8.2f",matrix[i][j]);
+                }
+                System.out.println();
+
+            }
+            return matrix;
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        } catch (NumberFormatException e){
+            System.out.println("Неверный формат элементов массива...");
+            return null;
+        }
 
 
+    }
 
 }
